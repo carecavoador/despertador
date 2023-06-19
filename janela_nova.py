3e15a95919ea5d13a074b0149e95c1c77b16d46a
Everton de Souza <e.rodrigo@outlook.com>
2023-06-19 16:59:41 -0300

    Database

--- janela_nova.py
+++ janela_nova.py
@@ -62,24 +62,23 @@ class TimerWindow(QtWidgets.QMainWindow):
         self.scroll.setWidget(self.painel_principal)
         self.setCentralWidget(self.scroll)
 
-    def remove_timer(self, timer: Alarme, widget) -> None:
+    def remove_timer(self, timer) -> None:
         self.db.remove_alarme(key=timer.id)
-        widget.close()
         self.update_timers()
 
     def update_timers(self) -> None:
-        self.timers = [Alarme(t[0], t[1], t[2], t[3]) for t in self.db.alarmes]
-        self.display_timers()
-
-
-    def display_timers(self) -> None:
+        self.timers = None
         while self.layout_timers.count() > 0:
             current = self.layout_timers.itemAt(0).widget()
             if current:
                 current.close()
             else:
                 break
+        self.timers = [Alarme(t[0], t[1], t[2], t[3]) for t in self.db.alarmes]
+        self.display_timers()
 
+
+    def display_timers(self) -> None:
         for timer in self.timers:
             widget = QtWidgets.QGroupBox()
             layout = QtWidgets.QGridLayout(widget)
@@ -87,7 +86,7 @@ class TimerWindow(QtWidgets.QMainWindow):
             label_description = QtWidgets.QLabel(f"<b>{timer.description}</b>")
             label_time = QtWidgets.QLabel(f"{timer.hour:02}:{timer.minutes:02}")
             btn_remove = QtWidgets.QPushButton(text="Remover alarme")
-            btn_remove.clicked.connect(lambda: self.remove_timer(timer, widget))
+            btn_remove.clicked.connect(lambda: self.remove_timer(timer))
 
             layout.addWidget(label_description, 0, 0)
             layout.addWidget(label_time, 1, 0)
@@ -97,7 +96,6 @@ class TimerWindow(QtWidgets.QMainWindow):
 
     def add_timer(self) -> None:
         new_timer = NewTimerDialog()
-
         if new_timer.exec():
             description = new_timer.description.text()
             if not description:
